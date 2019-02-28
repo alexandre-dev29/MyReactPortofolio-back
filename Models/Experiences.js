3a1e73bea5b445ccc171ce40ced01605b4d90556
Alexandre <axel.business29@gmail.com>
2019-02-28 09:25:36 +0200

    FInishing CRUD operation for experience and refractoring the experience model

--- Models/Experiences.js
+++ Models/Experiences.js
@@ -5,7 +5,9 @@ var experiencesSchema = new Schema({
     experience_id : Schema.ObjectId,
     experience_name: String,
     experience_percent: String, 
-    experience_category: String
+    experience_category: String,
+    experience_years: String,
+    experience_description: String
 })
 
 mongoose.model('experiences', experiencesSchema);
--- Routes/ExperienceRoute.js
+++ Routes/ExperienceRoute.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose")
 
 const router = express.Router();
+const {returnError, returnSuccess, returnCustom} = require('../Utils/respnseUtils')
+
+require('../Models/Experiences');
+require('../Models/Users');
+
+const experiences_model = mongoose.model('experiences');
+const user_model = mongoose.model('users');
 
 /**GET request for the home page */
 
@@ -8,4 +16,87 @@ router.get("/", (req, res) => {
   res.send("Voici la page d'accueil");
 });
 
+router.get("/get_experiences", (req, res) => {
+  const email = req.body.user.user_email;
+
+
+  user_model.find({user_email: email}, (error, result) => {
+    if (result.length > 0) {
+      experiences_model.find((err, all_data) => {
+        res.send(returnCustom("This is all experiences", all_data));
+      })
+    }else{
+      res.send(returnError("Invalid identification"));
+    }
+  })
+})
+
+router.post("/add_experiences", (req, res) => {
+  const email = req.body.user.user_email;
+  const experiences = req.body.experiences;
+
+
+  user_model.find({user_email: email}, (error, result) => {
+    if (result.length > 0) {
+      experiences.forEach(currentExp => {
+        const newExperience = new experiences_model({
+          experience_name: currentExp.experience_name,
+          experience_percent: currentExp.experience_percent, 
+          experience_category: currentExp.experience_category,
+          experience_years: currentExp.experience_years,
+          experience_description: currentExp.experience_description
+        });
+        newExperience.save();
+      });
+      res.send(returnSuccess("All experiences has been saved successfully"))
+    }else{
+      res.send(returnError("Invalid identification"));
+    }
+  })
+})
+
+router.put("/update_experiences", (req, res) => {
+  const email = req.body.user.user_email;
+  const experiences = req.body.experiences;
+
+
+  user_model.find({user_email: email}, (error, result) => {
+    if (result.length > 0) {
+      experiences.forEach(currentExp => {
+        experiences_model.updateOne({_id: currentExp._id}, 
+          {
+            experience_name: currentExp.experience_name,
+            experience_percent: currentExp.experience_percent, 
+            experience_category: currentExp.experience_category,
+            experience_years: currentExp.experience_years,
+            experience_description: currentExp.experience_description
+          },(error, result) => {});
+      });
+      res.send(returnSuccess("All these experiences has been update successfully"));
+    }else{
+      res.send(returnError("Invalid identification"));
+    }
+  })
+})
+
+router.delete("/delete_experiences", (req, res) => {
+  const email = req.body.user.user_email;
+  const experiences = req.body.experiences;
+
+  user_model.find({user_email: email}, (error, result) => {
+    if (result.length > 0) {
+      experiences.forEach(currentExp => {
+        experiences_model.deleteOne({_id: currentExp._id},
+          (err) => {
+              if(err){
+                  res.send(returnError("there was an error"));
+              }
+          });
+      });
+      res.send(returnSuccess("All these experiences has been succcessfully deleted"));
+    }else{
+      res.send(returnError("Invalid identification"));
+    }
+  })
+})
 module.exports = router;

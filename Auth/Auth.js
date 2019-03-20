c1a94fe4b4b2e0109af96ca25751c667791e178a
Alexandre <axel.business29@gmail.com>
2019-03-20 17:11:42 +0200

    adding security from env file

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -1,8 +1,6 @@
 //this file is create to provide verification of the header authorisation
 
-const API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ3ZWJzaXRlIjoiYXhlbG13ZW56ZS5jb20iLCJvd25lciI6ImF4ZWwgbXdlbnplIiwia2V5IjoiZG9uJ3QgZW50ZXIgdGhpcyBpcyBteSBwb3J0b2ZvbGlvIn0.HCAOtzWwVBWW7x3kq2dSQY9o_plolhAT0nFVawMyxNM";
-
+const API_KEY = process.env.API_KEY;
 //this method check the header and see if the request is valid for security issues
 const isAuthorize = (req, res, next) => {
   if (req.headers.key_auth && req.headers.key_auth === API_KEY) {

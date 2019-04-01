355e5a6cf3c6984060053c1cfe2464a707d091f3
Alexandre <axel.business29@gmail.com>
2019-04-01 15:59:09 +0200

    remove header params

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ let isAuthorize = require("./Auth/Auth");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     next();
